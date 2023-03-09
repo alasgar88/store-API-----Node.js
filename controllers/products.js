ae4a4e06980ae887be6e618b51d384ac36324123
Alasgar M <mahmudov.alasgar@gmail.com>
2023-03-10 01:44:27 +0400

    sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/products');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'a';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -21,8 +18,20 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryObject);
-  // const products = await Product.find(req.query);
+  let result = Product.find(queryObject);
+
+  // sort
+  if (sort) {
+    console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  // select fields
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
