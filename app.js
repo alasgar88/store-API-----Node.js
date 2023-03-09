89fc87d338ce32d7ea56b9252190ad16a833ecba
Alasgar M <mahmudov.alasgar@gmail.com>
2023-03-09 16:12:48 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+// async  errors
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorFoundMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorFoundMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connect to db
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
